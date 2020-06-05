2f7143d7d2e8c401ede13a91141cd48f79dc2d35
AhriZ08 <1061747368@qq.com>
2020-06-05 17:58:05 +0800

    短信服务以及redis缓存搭建

---
+++ Users/WebBack/dfoods/src/main/java/com/example/deliciousfoods/service/AliSmsService.java
@@ -0,0 +1,56 @@
+package com.example.deliciousfoods.service;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class AliSmsService {
+
+    public String sendSms(String telNum) throws ClientException {
+        //替换成你的AK
+        final String accessKeyId = "LTAI4G4duWs2PdWDHnFo8MhW";//你的accessKeyId
+        final String accessKeySecret = "WHXlTFlDb2HyzbYKQV1cqM6unK6CpI";//你的accessKeySecret
+        //初始化ascClient,暂时不支持多region（请勿修改）
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+        IAcsClient acsClient = new DefaultAcsClient(profile);
+        //组装请求对象
+        SendSmsRequest request = new SendSmsRequest();
+        //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为00+国际区号+号码，如“0085200000000”
+        request.setPhoneNumbers(telNum);
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName("美食家APP");
+        //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
+        request.setTemplateCode("SMS_192575201");
+        //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
+        //request.setTemplateParam("{\"code\":\"988756\"}");
+        String msgCode = getMsgCode();
+        request.setTemplateParam("{\"code\":\"" + msgCode + "\"}");
+        //请求失败这里会抛ClientException异常
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+        if (sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
+            //请求成功
+            System.out.println("短信发送成功");
+        } else {
+            System.out.println("短信发送失败");
+        }
+        return msgCode;
+    }
+    private static String getMsgCode() {
+        int n = 6;
+        StringBuilder code = new StringBuilder();
+        Random ran = new Random();
+        for (int i = 0; i < n; i++) {
+            code.append(Integer.valueOf(ran.nextInt(10)).toString());
+        }
+        return code.toString();
+    }
+}
